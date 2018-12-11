46687b84c6b2cfb01a301d1ca044e7b630e50d4a
Viivi <t9juvi@students.oamk.fi>
2018-12-11 08:37:27 +0200

    truth_table

--- truth_table.js
+++ truth_table.js
@@ -1,16 +1,31 @@
-function submitTable()
-{
-    var p = document.forms["myForm"]["selectP"].value;
-    var q = document.forms["myForm"]["selectQ"].value;
-    var operator = document.forms["myForm"]["operator"].value;
-
-    
-
-    if (operator= "and"){
-       return p && q;
-    }
-
-    if (operator= "or"){
-        return p || q;
-    }
-}
+// These are x and y for each row of the table
+const initialValues = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+  ]
+  
+  // This holds the html table
+  const table = document.getElementById('table')
+  
+  // With this loop we go through all the rows of the initial values and calculate AND, OR, NAND and NOR
+  for (let i = 0; i < initialValues.length; i++) {
+    const x = initialValues[i][0]
+    const y = initialValues[i][1]
+  
+    // We add this HTML to the table on every iteration
+    // Use Number() to transform from true or false to 1 or 0
+    table.innerHTML += `
+      <tr>
+        <td>${x}</td>
+        <td>${y}</td>
+        <td>${Number(x && y)}</td>
+        <td>${Number(x || y)}</td>
+        <td>${Number(!(x && y))}</td>
+        <td>${Number(!(x || y))}</td>
+        <td>${Number((x && !y) || (!x && y))}</td>
+      </tr>
+    `
+  }
+  
