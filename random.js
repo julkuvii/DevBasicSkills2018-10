0d9c41f7cabf8daaf49c399f7b23477abf1d9ef7
yeksam <45554100+yeksam@users.noreply.github.com>
2018-12-15 22:43:28 +0200

    Add files via upload

--- random.js
+++ random.js
@@ -1,10 +1,18 @@
 
   function generateRandomNumber(){
-   
+    var arr = [];
     var min = parseInt(document.getElementById('min').value);
     var max = parseInt(document.getElementById('max').value);
-    var rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.getElementById('display').innerText = rand;
-  
- }
-  
+    var range= parseInt(document.getElementById('range').value);
+    while(arr.length < range) {
+      var random = Math.floor(Math.random() * (max - min + 1)) + min;
+      if(arr.indexOf(random) === -1) {
+        arr.push(random);
+      }
+    }
+
+    document.getElementById("display").innerHTML = arr;
+
+    }
+
+    
